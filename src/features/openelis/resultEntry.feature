97aed26eb4a4d26be65d551df73f90a341fa278d
mozzy11 <mozzymutesa@gmail.com>
2021-06-28 15:36:26 +0300

    Searching for test result entry forms #19

--- src/features/openelis/resultEntry.feature
+++ src/features/openelis/resultEntry.feature
@@ -18,3 +18,22 @@ Then Results by Test, Date or Status search form displays
 Examples:
 |    unitType   |
 |Molecular Bio A|
+
+@results
+Scenario Outline:Searching for test result entry forms
+When User Selects Results --> Search --> By Patient
+Then Results search form displays
+And Search button deactivated until search text entered
+When User Searches by Last name "<lastName>"
+Then Search by Last name yields all patients with matching last name
+When User Searches by First name "<FirstName>"
+Then Search by First name yields all patients with matching first name
+When User Searches by Patient Identification Code "<patientId>"
+Then Search by Patient Id yields all patients with matching patient id
+When User Selects Results --> Search --> By Order
+Then Result accession number search form displays
+When User Searches by AccesionNumber "<accesionNumber>"
+Then Search by Lab Number yields results for known accession number
+Examples:
+|patientId  |lastName|FirstName|accesionNumber|
+|HT7823DAAAZ|Foxy    | McGee   |1234519000002 |
--- src/test/java/org/openelisglobal/qaframework/automation/page/SearchResultsByOrderPage.java
+++ src/test/java/org/openelisglobal/qaframework/automation/page/SearchResultsByOrderPage.java
@@ -4,11 +4,18 @@ import org.openqa.selenium.By;
 
 public class SearchResultsByOrderPage extends Page {
 
-	private static final String PAGE_PATH = "/PatientResults.do";
+	private static final String PAGE_PATH = "/AccessionResults.do";
 
 	private static final By SEARCH_FORM = By.id("searchDiv");
 
 	private static final String PATH_HOME = "/Dashboard.do";
+
+	private static final By FIELD_LAB_NO_SEARCH = By.id("searchAccessionID");
+
+	private static final By LABEL_ACCESION_NOT_FOUND = By
+			.xpath("//*[@id='mainForm']/table/tbody/tr[1]/td/center/h1");
+
+	private static By BUTTON_ACCESION_NUMBER_SERCH = By.id("retrieveTestsID");
 
 	public SearchResultsByOrderPage(Page parent) {
 		super(parent);
@@ -26,5 +33,17 @@ public class SearchResultsByOrderPage extends Page {
 	public HomePage goToHomePage() {
 		this.goToPage(PATH_HOME);
 		return new HomePage(this);
+	}
+
+	public void enterAccesionNUmber(String accesionNumber) {
+		setText(FIELD_LAB_NO_SEARCH, accesionNumber);
+	}
+
+	public Boolean accesionNumberNotFoundDisplays() {
+		return hasElement(LABEL_ACCESION_NOT_FOUND);
+	}
+
+	public void clickAccesionNumberSearch() {
+		clickOn(BUTTON_ACCESION_NUMBER_SERCH);
 	}
 }
