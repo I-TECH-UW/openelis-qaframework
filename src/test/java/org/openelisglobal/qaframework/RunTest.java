473edfd3da229fddb7e00bb50339f95d80c86109
abertnamanya <abertnamanya@gmail.com>
2023-01-24 15:24:19 +0300

    Automate test case for checking Test Management menu list items

--- src/test/java/org/openelisglobal/qaframework/RunTest.java
+++ src/test/java/org/openelisglobal/qaframework/RunTest.java
@@ -11,25 +11,27 @@ import org.junit.runner.RunWith;
         // defining the definition steps package
         glue = "org.openelisglobal.qaframework.automation", plugin = { "pretty"},monochrome = true)
 public class RunTest {
-	
+
 	public class HOOK {
-		
+
 		public static final String LOGIN = "@login";
-		
+
 		public static final String ORDER = "@order";
-		
+
 		public static final String RESULT = "@results";
-		
+
 		public static final String VALIDATE = "@validate";
-		
+
 		public static final String REPORT = "@report";
-		
+
 		public static final String PATIENT_ENTRY = "@patientEntry";
 
 		public static final String MODIFY_ORDER = "@modifyOrder";
 
 		public static final String REFERAL_WORK_FLOW = "@referral";
-		
+
 		public static final String EMR_LIS = "@emrLis";
+
+		public static final String TEST_MGT = "@testManagement";
 	}
 }
