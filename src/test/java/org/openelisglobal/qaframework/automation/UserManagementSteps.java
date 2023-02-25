78737df08eaa229ddb7e7b4d2299bfe210967e78
abertnamanya <abertnamanya@gmail.com>
2023-02-25 23:23:40 +0300

    Automation of usermanagement testCase

--- src/test/java/org/openelisglobal/qaframework/automation/UserManagementSteps.java
+++ src/test/java/org/openelisglobal/qaframework/automation/UserManagementSteps.java
@@ -1,11 +1,13 @@
 package org.openelisglobal.qaframework.automation;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openelisglobal.qaframework.RunTest;
 import org.openelisglobal.qaframework.automation.page.AdminPage;
@@ -62,8 +64,65 @@ public class UserManagementSteps extends TestBase {
 		assertTrue(userManagementPage.containsText("Add User"));
 	}
 
-	/** TODO: 26/01/2023
-	*  user management is incomplete due to waiting for fix
-	  updates from dev team
-	 **/
+	@Then("username,password, repeat password,Firstname,lastname and Password Expiration Date Should be required")
+	public void usernamePasswordRepeatPasswordFirstnameLastnameAndPasswordExpirationDateShouldBeRequired() {
+		assertEquals(userManagementPage.getUsernameRequiredClass(), "requiredlabel");
+		assertEquals(userManagementPage.getPasswordRequiredClass(), "requiredlabel");
+		assertEquals(userManagementPage.getRepeatPasswordRequiredClass(), "requiredlabel");
+		assertEquals(userManagementPage.getPasswordExpiryDateRequiredClass(), "requiredlabel");
+		assertEquals(userManagementPage.getFirstNameRequiredClass(), "requiredlabel");
+		assertEquals(userManagementPage.getLastnameRequiredClass(), "requiredlabel");
+	}
+
+	@And("User enters username {string}")
+	public void userEntersUsername(String username) {
+		userManagementPage.enterUsername(username);
+	}
+
+	@And("User enters first name {string}")
+	public void userEntersFirstName(String firstName) {
+		userManagementPage.enterFirstName(firstName);
+	}
+
+	@And("User enters last name {string}")
+	public void userEntersLastName(String lastName) {
+		userManagementPage.enterLastName(lastName);
+	}
+
+	@And("User enters password {string}")
+	public void userEntersPassword(String password) {
+		userManagementPage.enterPasswordAndConfirmPassword(password);
+	}
+
+	@Then("Assign all the roles and permissions to the newly created user")
+	public void assignAllTheRolesAndPermissionsToTheNewlyCreatedUser() {
+		assertTrue(userManagementPage.globalRoleExists());
+		assertTrue(userManagementPage.allPermissionsCheckBoxExists());
+		userManagementPage.assignGlobalRole();
+		userManagementPage.assignAllPermissions();
+	}
+
+	@And("User Clicks save button to create user information")
+	public void userClicksSaveButtonToCreateUserInformation() throws InterruptedException {
+		userManagementPage.clickSaveButton();
+		Thread.sleep(1000);
+	}
+
+	@Then("User logs out and logins with the newly created account {string} and {string}")
+	public void userLogsOutAndLoginsWithTheNewlyCreatedAccountAnd(String username, String password)
+			throws InterruptedException {
+		homePage = userManagementPage.goToHomePage();
+		if (userManagementPage.alertPresent()) {
+			userManagementPage.acceptAlert();
+		}
+		loginPage = homePage.ClickLogOut();
+		assertTrue(loginPage.containsText("Login"));
+
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+		loginPage.getLoginButton().click();
+		Thread.sleep(100);
+		assertTrue(homePage.containsText("Namanya Abert"));
+	}
+
 }
